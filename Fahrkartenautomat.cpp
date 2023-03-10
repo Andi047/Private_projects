ca300a7bb012f27c5dd696db482a6ea91ab98fa7
Andreas Oswald <40559257+Andi047@users.noreply.github.com>
2023-03-10 16:02:28 +0100

    add exception handling

--- Fahrkartenautomat.cpp
+++ Fahrkartenautomat.cpp
@@ -2,39 +2,54 @@
 #include <string>
 using namespace std;
 
-double fahrk_A = 3.99;
-double fahrk_B = 4.5;
-double fahrk_C = 5.99;
+const double fahrk_A = 3.99;
+const double fahrk_B = 4.5;
+const double fahrk_C = 5.99;
 double preis;
 
 char eingabe;
 double einwurf;
 double einwurf_tmp;
+bool isValid = false;
 
 double wechselgeld;
 void calcWechsel(double coins);
 
 
-int main() {
-    cout << "Bitte wählen Sie eine Zone (A / B / C):" << endl;
-    cin >> eingabe;
+int main()
+{
+    while (isValid != true) {
 
-    cout << "Gewählte Zone: " << eingabe << endl;
+        cout << "Bitte wählen Sie eine Zone (1 / 2 / 3):" << endl;
+        cout << "Zum Abbrechen bitte 'q' drücken.";
+        cin >> eingabe;
 
-    switch (eingabe) {
-        case 'A':
-            preis = fahrk_A;
-            break;
-        case 'B':
-            preis = fahrk_B;
-            break;
-        case 'C':
-            preis = fahrk_C;
-            break;
-        default:
-            cout << "Falsche Eingabe. Programm Ende.";
-            exit(0);
+        cout << "Gewählte Zone: " << eingabe << endl;
+
+        switch (eingabe) {
+            case '1':
+                preis = fahrk_A;
+                isValid = true;
+                break;
+            case '2':
+                preis = fahrk_B;
+                isValid = true;
+                break;
+            case '3':
+                preis = fahrk_C;
+                isValid = true;
+                break;
+            case 'q':
+                cout << "Abbruch Programm.";
+                isValid = true;
+                exit(0);
+                break;
+            default:
+                cout << "Ungültigte Eingabe. ";
+        }
+
     }
+    
 
     cout << "Bitte zahlen Sie: " << preis << endl;
     cout << "Geben Sie dabei den Wert so ein: X.XX" << endl;
@@ -53,10 +68,11 @@ int main() {
 
     calcWechsel(einwurf_tmp);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-void calcWechsel(double coins) {
+void calcWechsel(double coins)
+{
     wechselgeld = coins - preis;
     cout << "Sie erhalten als Wechselgeld: " << wechselgeld << endl;
 }
